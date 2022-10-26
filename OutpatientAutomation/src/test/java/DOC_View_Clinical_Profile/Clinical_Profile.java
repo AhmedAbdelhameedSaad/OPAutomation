0d47ae6774a9f1b2b9d9582e67d15885f5b30fcc
Dalia <Dalia@192.168.1.5>
2022-10-26 10:54:05 +0200

    gtrggggg

--- OutpatientAutomation/src/test/java/DOC_View_Clinical_Profile/Clinical_Profile.java
+++ OutpatientAutomation/src/test/java/DOC_View_Clinical_Profile/Clinical_Profile.java
@@ -18,7 +18,7 @@ public class Clinical_Profile {
 
 			Thread.sleep(3000);
 
-			// patients Vitals and Allergies fghhh
+			// patients Vitals and Allergies fghhh dadadfgshshs
             
 			By Allergies = By.xpath(
 					"/html/body/app-root/app-crm/div/div/app-patient-view/div/app-dashboard/div/gridster/gridster-item[2]/app-widget/div/div[1]/app-clinical-profile/div[1]/div[2]/div[1]/p");
