31b9067ba05b4fbafd36cda2ad401c9e7bcef2d6
Rehab Hussein <116155457+RehabDBLX@users.noreply.github.com>
2022-11-29 10:55:58 +0200

    Internal & External Referral

--- OutpatientAutomation/src/test/java/testCases/OP_TC_235_259_779_Verify_OPConsultationForm.java
+++ OutpatientAutomation/src/test/java/testCases/OP_TC_235_259_779_Verify_OPConsultationForm.java
@@ -12,7 +12,7 @@ import opd_new_documents_forms.OPConsultation_Form;
 import opd_patient.Physician_SearchFor_Patient;
 import testBase.*;
 
-public class OP_TC_259_779_Verify_OPConsultationForm extends HP_Test_Base
+public class OP_TC_235_259_779_Verify_OPConsultationForm extends HP_Test_Base
 
 {
 
@@ -30,11 +30,11 @@ public class OP_TC_259_779_Verify_OPConsultationForm extends HP_Test_Base
 			driver.findElement(ct).click();
 			
 		}catch (InterruptedException ex) {
-			Logger.getLogger(OP_TC_259_779_Verify_OPConsultationForm.class.getName()).log(Level.SEVERE, null, ex);
+			Logger.getLogger(OP_TC_235_259_779_Verify_OPConsultationForm.class.getName()).log(Level.SEVERE, null, ex);
         }
 		
 		Physician_SearchFor_Patient find_patient = new Physician_SearchFor_Patient(driver);
-		find_patient.search_by_Name("Tarek");
+		find_patient.physician_search_by_Name("Tarek");
 		
 
 		
